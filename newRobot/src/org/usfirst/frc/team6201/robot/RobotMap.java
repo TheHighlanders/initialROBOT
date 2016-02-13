50c8338ddcd907dfb7cb2ac663ca89839fcc3f4d
Max Nadeau <nadeau.max@gmail.com>
2016-02-13 10:49:19 -0500

    Fixed

--- newRobot/src/org/usfirst/frc/team6201/robot/RobotMap.java
+++ newRobot/src/org/usfirst/frc/team6201/robot/RobotMap.java
@@ -22,13 +22,13 @@ public class RobotMap {
 	
 	// TankdriveCmd
 	public static final int XBOX = 0;
-	public static final int ARCADE = 1;
 	public static final int UP_AXIS = 0;
 	public static final int XBOX_RIGHT = 0;
 	public static final int XBOX_LEFT = 0;
 	
 	
 	// ArcadeDriveCmd
+	public static final int ARCADE = 1;
 	public static final int LOGITECH = 1;
 	public static final int LOGITECH_X_AXIS = 0;
 	public static final int LOGITECH_Y_AXIS = 1;
