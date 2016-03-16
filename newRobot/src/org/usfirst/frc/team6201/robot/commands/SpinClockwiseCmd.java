7e03ca16e946bdf485baeb224641e730a0eea1dc
Adrianamm <adrianammassie@gmail.com>
2016-03-16 15:34:58 -0400

    Added files via upload

---
+++ newRobot/src/org/usfirst/frc/team6201/robot/commands/SpinClockwiseCmd.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team6201.robot.commands;
+
+	import org.usfirst.frc.team6201.robot.Robot;
+	import org.usfirst.frc.team6201.robot.RobotMap;
+	import edu.wpi.first.wpilibj.command.Command;
+
+	/**
+	 *@author Adriana Massie
+	 *
+	 *
+	 *Tells the robot to spin clockwise 
+	 */
+	public class SpinClockwiseCmd() extends Command {
+
+	    public SpinClockwiseCmd() {
+	        // Use requires() here to declare subsystem dependencies
+	         requires(Robot.dt);
+	    }
+
+	    
+	   //Called just before this Command runs the first time
+	    protected void initialize() {
+	    }
+
+	    protected void execute() {	
+	    	Robot.dt.driveLR(0.95,-0.95 );
+	    }
+
+	    // Make this return true when this Command no longer needs to run execute()
+	    protected boolean isFinished() {
+	        return false;
+	    }
+
+	    // Called once after isFinished returns true
+	    protected void end() {
+	    	Robot.dt.stop();
+	    }
+
+	    // Called when another command which requires one or more of the same
+	    // subsystems is scheduled to run
+	    protected void interrupted() {
+	    	
+	    	this.end();
+	    
+	    }
+}
