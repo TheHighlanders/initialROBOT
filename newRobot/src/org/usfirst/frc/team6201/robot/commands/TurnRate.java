60a9d16a9c0da3718571ed40eca7a4558f0b1e66
dmatthewsshs <david.matthews@mosaicsystems.com>
2016-10-19 11:16:31 -0400

    Initial Draft of TurnRateCmd.java is complete. It narrows in on a rate of turning. This command does not currently support user adjustment input.
    I currently am thinking that We modify the DriveTrain subsystem to have a boolean variable to hold if we want to accept user input. Then use a button or slider on the joystick to control how much control on the final output the user has.
    
    Signed-off-by: dmatthewsshs <david.matthews@mosaicsystems.com>

--- newRobot/src/org/usfirst/frc/team6201/robot/commands/TurnRate.java
+++ newRobot/src/org/usfirst/frc/team6201/robot/commands/TurnRate.java
@@ -5,7 +5,8 @@ import org.usfirst.frc.team6201.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- * This class is used to get the robot to turn at a specified rate of turning in degrees per second.
+ * This class is used to get the robot to turn at a specified rate of turning in
+ * degrees per second.
  * 
  * @author David Matthews
  * 
@@ -13,35 +14,52 @@ import edu.wpi.first.wpilibj.command.Command;
 public class TurnRate extends Command {
 
 	private double targetTurnRate;
-	
+	private double leftMotorSpeed = 0;
+	private double rightMotorSpeed = 0;
+
 	/**
 	 * 
-	 * @param rateOfTurning	The rate that the robot should turn at.
+	 * @param rateOfTurning
+	 *            The rate that the robot should turn at.
 	 */
-    public TurnRate(int rateOfTurning) {
-    	requires(Robot.dt);
-    	targetTurnRate = rateOfTurning;
-    }
+	public TurnRate(int rateOfTurning) {
+		requires(Robot.dt);
+		targetTurnRate = rateOfTurning;
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	private double error() {
+		return (Robot.dt.getGyroRate() - targetTurnRate);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double currError = error();
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+		if (currError >= 0) {
+			
+			leftMotorSpeed -= Math.min(currError * 0.1, 0.5);
+			rightMotorSpeed +=  Math.min(currError * 0.1, 0.5);
+		} else {
+			leftMotorSpeed +=  Math.min(currError * 0.1, 0.5);
+			rightMotorSpeed -=  Math.min(currError * 0.1, 0.5);
+		}
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
