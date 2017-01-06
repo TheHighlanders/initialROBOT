c1e311fde4725f7213525defe29060172e09eb14
David Matthews <david.matthews@mosaicsystems.com>
2017-01-06 07:53:37 -0500

    Yesterday, when I was using Ctr+C to kill the recorder process, the logged data was not being saved. WireShark confirms that the data was being sent over the network. the likely cause is that the data on the recorder was in memory and the operating system was not told to save it to disk before the process was killed. I have attempted to fix this. Will be testing soon
    Signed-off-by: David Matthews <david.matthews@mosaicsystems.com>

--- RemoteDataLogger/DataLoggerRecorder.java
+++ RemoteDataLogger/DataLoggerRecorder.java
@@ -48,6 +48,7 @@ public class DataLoggerRecorder {
 				
 				//process string appropriately.
 				saveData(fw, dataString);
+                fw.flush();
 			}
 			fw.close();
 			inputSocket.close();
